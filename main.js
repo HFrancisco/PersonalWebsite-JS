a95b5bbcf6e7e865863aa8985c77143b16cf0b92
Harold Francisco <hfrancisco@you-source.com>
2020-06-21 17:51:52 +0800

    Initial commit. Used a modified jumbotron template and ported card template from prev personal website design

---
+++ main.js
@@ -0,0 +1,16 @@
+var jobTitle = "junior full-stack";
+var jobTitleHtml = document.getElementById('jobTitle').innerHTML = jobTitle;
+
+var employerDict = {
+    name: "YouSource Inc.",
+    link: "https://www.you-source.com/"
+};
+document.getElementById('employer').innerHTML = employerDict.name;
+
+var locationDict = {
+    city: "Makati City",
+    country: "Philippines",
+    countryIso: "PHL"
+};
+document.getElementById('city').innerHTML = locationDict.city;
+document.getElementById('countryIso').innerHTML = locationDict.countryIso;
